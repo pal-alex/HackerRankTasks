e15f73097d6a12f97328d4b0dc8ffb27adf91e39
pal-alex <palchikovskiy.alexander@gmail.com>
2020-10-28 21:58:53 +0100

    #1

---
+++ FunctionalProgramming/HelloWorld.ex
@@ -0,0 +1,11 @@
+#Easy #5
+#https://www.hackerrank.com/challenges/fp-hello-world/problem
+
+defmodule Solution do
+  #Enter your code here. Read input from STDIN. Print output to STDOUT
+
+   def print(), do: IO.puts("Hello World")
+
+end
+
+Solution.print()
